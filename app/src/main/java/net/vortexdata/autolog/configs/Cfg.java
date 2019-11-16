95e94018abd58647c516e1d970c6d28d965a1469
Klausmd5 <klaus@vortexdata.net>
2019-11-16 21:33:47 +0100

    updates to version 2.1.1 bugfixes

--- app/src/main/java/net/vortexdata/autolog/configs/Cfg.java
+++ app/src/main/java/net/vortexdata/autolog/configs/Cfg.java
@@ -9,7 +9,7 @@ public class Cfg {
     public static boolean autoConnect = false;
     public static boolean openTab = false;
 
-    public static String version = "2.1.0";
+    public static String version = "2.1.1 BETA";
     public static String logURL = "http://10.10.0.251:8002/index.php?zone=cp_htl";
     public static String newsFeed = "https://src.vortexdata.net/modules/api/autolog.php";
     //public static String logURL = "http://cms.vortexdata.net/services/BaseCMS.php";
